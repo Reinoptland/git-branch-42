33840853d11f03b09f3c6ef21e713543a39b30a6
Reinoptland <reinoptland@gmail.com>
2020-07-20 13:59:15 +0200

    Add title to main page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Git best practice</h1>
       <header className="App-header">
         <img
           src="https://camo.githubusercontent.com/c0cd7991caeb2b8b037fd2088dabc216b4d50f3f/68747470733a2f2f66656174757265666c6167732e696f2f77702d636f6e74656e742f75706c6f6164732f323031382f30332f666561747572656272616e6368696e67776974686f75742e6a7067"
